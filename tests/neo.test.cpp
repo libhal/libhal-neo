08238f73c4ef6c6769610c784c79bae2e92eef57
acarrou <dridriswim@gmail.com>
2023-10-15 15:05:50 -0700

    :green_heart: fixes run_testst

--- tests/neo.test.cpp
+++ tests/neo.test.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <libhal-neo/neo.hpp>
+#include <libhal-neo/neo-m9n.hpp>
 
 #include <boost/ut.hpp>
 
