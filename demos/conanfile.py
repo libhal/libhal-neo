e261f471eb5822aaf1fbc70343acbf9d1eaed309
Khalil Estell <kammce@google.com>
2023-11-27 18:45:52 -0800

    :arrow_up: upgrade libhal post CCI update

--- demos/conanfile.py
+++ demos/conanfile.py
@@ -29,7 +29,7 @@ class demos(ConanFile):
         if str(self.options.platform).startswith("lpc40"):
             self.requires("libhal-lpc40/[^2.1.3]")
         self.requires("libhal-neo/0.0.1")
-        self.requires("libhal-util/[^3.0.0]")
+        self.requires("libhal-util/[^3.0.1]")
 
     def layout(self):
         platform_directory = "build/" + str(self.options.platform)
